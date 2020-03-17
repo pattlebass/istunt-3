58c732b80ce1a576d1e6c1c5d22df6c8b14e552b
pattlebass <49322676+pattlebass@users.noreply.github.com>
2020-03-17 20:09:25 +0200

    Add files

--- iStunt 3/assets/player/player.gd
+++ iStunt 3/assets/player/player.gd
@@ -3,8 +3,9 @@ extends RigidBody2D
 var turning_speed = 5
 
 func _physics_process(delta):
-	if Input.is_action_pressed("left"):
-		angular_velocity = lerp(angular_velocity, -turning_speed, delta * 20)
-	if Input.is_action_pressed("right"):
-		angular_velocity = lerp(angular_velocity, turning_speed, delta * 20)
-	print(angular_velocity)
+	for ray in $rays.get_children():
+		if !ray.is_colliding():
+			if Input.is_action_pressed("left"):
+				angular_velocity = lerp(angular_velocity, -turning_speed, delta * 20)
+			if Input.is_action_pressed("right"):
+				angular_velocity = lerp(angular_velocity, turning_speed, delta * 20)
